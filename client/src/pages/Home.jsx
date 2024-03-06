b835ed1e66674caa122749b3eeaa3a48cbdfec89
Retr078 <cgpin78@outlook.com>
2024-03-06 15:12:30 +0800

    removing the config file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import {useSnapshot} from 'valtio';
+
+
+import state from '../store';
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion';
 
 const Home = () => {
+    const snap = useSnapshot(state);
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        {snap.intro && (
+            <motion.section className="home" {...slideAnimation('left')}>
+                <motion.header>
+                    
+                </motion.header>
+            </motion.section>
+        )}
+    </AnimatePresence>
   )
 }
 
