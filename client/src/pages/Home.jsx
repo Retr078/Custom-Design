aaf123473e5b28b33718f0ead1d0ba917bda7da5
Retr078 <cgpin78@outlook.com>
2024-03-06 15:34:45 +0800

    adding more animations

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,8 +21,15 @@ const Home = () => {
                     <img src="./threejs.png" alt="logo" className="w-8 h-8 object-contain"/>
                 </motion.header>
 
-                <motion.div>
-
+                <motion.div className='home-content' {...headContainerAnimation}>
+                    <motion.div {...headTextAnimation}>
+                        <h1 className='head-text'>LET'S <br className='xl:block hidden'/> DO IT</h1>
+                    </motion.div>
+                    <motion.div {...headContentAnimation}>
+                        <p className='max-w-md fontnormal text-grey-600 text-base'>
+                            Create yout unique and exclusive shirt with our brand-new 3D custommization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+                        </p>
+                    </motion.div>
                 </motion.div>
             </motion.section>
         )}
